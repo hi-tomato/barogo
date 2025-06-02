f63b3c7cd5db7b09c0a1b481b833473d1576cda2
jun <tmdwns0927@naver.com>
2025-06-03 02:37:00 +0900

    file: 디렉토리 구조 변경

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,11 +1,18 @@
 import React, { ReactNode } from "react";
 import Footer from "@/app/components/layout/Footer";
 
-export default function AfterLoginLoot({ children }: { children: ReactNode }) {
+export default function AfterLoginLoot({
+  children,
+  modal,
+}: {
+  children: ReactNode;
+  modal: ReactNode;
+}) {
   return (
     <div>
       <main className="pb-20">{children}</main>
       <Footer />
+      {modal}
     </div>
   );
 }
