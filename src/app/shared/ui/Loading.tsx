e1a142bd8e2cc5b54baa623a9b12a1f5a5b88a9f
이승준 <tmdwns092727@nate.com>
2025-07-06 00:54:03 +0900

    rename: 파일명 수정

--- src/app/shared/ui/Loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="py-12 text-center">
-      <div className="mx-auto mb-4 h-8 w-8 animate-spin rounded-full border-2 border-blue-500 border-t-transparent" />
-      <p className="text-gray-500">로딩중...</p>
-    </div>
-  );
-}
