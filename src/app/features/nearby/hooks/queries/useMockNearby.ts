779d449d690973a9cd91f57697f60304b77b6532
이승준 <tmdwns092727@nate.com>
2025-07-07 16:25:07 +0900

    refactor: useMockNearby Type Error 수정

--- src/app/features/nearby/hooks/queries/useMockNearby.ts
+++ src/app/features/nearby/hooks/queries/useMockNearby.ts
@@ -1,13 +1,12 @@
-import { queryKeys } from "@/app/shared/lib/queryKeys";
-import { kakaoApiService } from "@/app/features/nearby/services/kakaoApi";
-import { Location } from "@/app/types";
-import { useQuery } from "@tanstack/react-query";
+import { queryKeys } from '@/app/shared/lib/queryKeys';
+import { kakaoApiService } from '@/app/features/nearby/services/kakaoApi';
+import { Location } from '@/app/shared/types';
+import { useQuery } from '@tanstack/react-query';
 
 export const useMockNearby = (location: Location | null) => {
-  // TODO: Type Error 고치기
   return useQuery({
     queryKey: location ? queryKeys.restaurant.nearby(location) : [],
-    queryFn: () => kakaoApiService.getNearbyRestaurants(location),
+    queryFn: () => kakaoApiService.getNearbyRestaurants(location as Location),
     enabled: !!location,
     staleTime: 1000 * 60 * 5,
     retry: 2,
