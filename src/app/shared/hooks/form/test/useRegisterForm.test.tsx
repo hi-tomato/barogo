c1b918866975328f96a99a4c0a89a5ebb46e4983
이승준 <tmdwns092727@nate.com>
2025-07-21 10:49:22 +0900

    refactor: 회원가입 폼 테스트 코드 수정
    - useRegisterForm.ts (이메일 유효성 검사 추가)
    - useRegisterForm.test.tsx (이메일 유효성 검사 테스트 추가)

--- src/app/shared/hooks/form/test/useRegisterForm.test.tsx
+++ src/app/shared/hooks/form/test/useRegisterForm.test.tsx
@@ -24,26 +24,39 @@ const wrapper = ({ children }: { children: ReactNode }) => (
 );
 
 describe('회원가입 폼 테스트', () => {
+  let result: {
+    current: ReturnType<typeof useRegisterForm>;
+  };
+
   beforeEach(() => {
     vi.clearAllMocks();
+
+    const { result: hookResult } = renderHook(() => useRegisterForm(), {
+      wrapper,
+    });
+    result = hookResult;
+
+    act(() => {
+      result.current.register('name', result.current.VALIDATE_RULES.name);
+      result.current.register('email', result.current.VALIDATE_RULES.email);
+      result.current.register(
+        'password',
+        result.current.VALIDATE_RULES.password
+      );
+    });
   });
 
-  it('회원가입 폼 제출 시, 회원가입 폼이 전달되면서 모달이 열린다', async () => {
-    const { result } = renderHook(() => useRegisterForm(), { wrapper });
-
+  it('회원가입 인풋을 올바르게 작성하면, 회원가입 모달이 전달되면서 이동한다', async () => {
     await act(async () => {
-      result.current.setValue('name', 'tomato');
+      result.current.setValue('name', '이승준');
       result.current.setValue('email', 'admin@naver.com');
       result.current.setValue('password', 'Password1234!!');
-      result.current.setValue('confirmPassword', 'Password1234!!');
-      result.current.setValue('terms', true);
-
       await result.current.handleSubmit();
     });
 
     expect(mockSignUp).toHaveBeenCalledWith(
       {
-        name: 'tomato',
+        name: '이승준',
         email: 'admin@naver.com',
         password: 'Password1234!!',
       },
@@ -58,5 +71,23 @@ describe('회원가입 폼 테스트', () => {
     });
 
     expect(result.current.showSuccessModal).toBe(true);
+  });
+
+  it('회원가입 폼을 잘못 작성하면 회원가입 폼이 전달되지 않는다', async () => {
+    await act(async () => {
+      result.current.setValue('name', 'tomato');
+      result.current.setValue('email', 'invalid-email.com');
+      result.current.setValue('password', 'Password1234!!');
+      result.current.setValue('confirmPassword', 'Password1234!!');
+      result.current.setValue('terms', true);
+
+      await result.current.handleSubmit();
+    });
+
+    expect(mockSignUp).not.toHaveBeenCalled();
+    expect(result.current.errors.email).toBeDefined();
+    expect(result.current.errors.email?.message).toBe(
+      '올바른 이메일 형식이 아닙니다'
+    );
   });
 });
