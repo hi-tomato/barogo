a11fcfb819edddfd2da201d944f0929e3f660410
이승준 <tmdwns092727@nate.com>
2025-07-22 20:33:05 +0900

    fix: baropotChat Token 전달 방법 수정
    - 기존 Header 토큰 방식은 전달되지 않는 것을 확인
    - 따라서 URL 파라미터로 전달하도록 수정

--- src/app/shared/services/baropotChatService.ts
+++ src/app/shared/services/baropotChatService.ts
@@ -23,7 +23,7 @@ export class BaropotChatService {
   async connect(token: string): Promise<void> {
     return new Promise((resolve, reject) => {
       this.socket = io(API_URL, {
-        extraHeaders: { token },
+        query: { token },
         transports: ['websocket'],
         forceNew: true,
       });
