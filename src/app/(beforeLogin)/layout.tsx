a812eb19cb234d0ee9a51448c2d17c441b1be4c0
이승준 <tmdwns092727@nate.com>
2025-07-22 16:55:33 +0900

    style: 메인 페이지, 로그인 페이지 배경 색상 수정

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -4,7 +4,7 @@ import ToastContextProvider from '@/app/shared/ui/toast/ToastContext';
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-[#d3dbe2]">
+    <div className="bg-gradient-to-br from-white to-gray-50">
       <ToastContextProvider>
         {children}
         <ToastContainer />
