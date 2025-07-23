b50595e5ec564885111c3a777c29ffdf6d671433
이승준 <tmdwns092727@nate.com>
2025-07-23 18:46:37 +0900

    fix: 경로 오류 설정
    - 바로팟 상세페이지 이동 시, 전체 리스트 페이지로 이동하도록 설정
    - 상세페이지 이동 시, 토스트 메시지 추가

--- src/app/features/map/components/popup/PopupActions.tsx
+++ src/app/features/map/components/popup/PopupActions.tsx
@@ -1,36 +1,41 @@
-"use client";
-import { Restaurant } from "@/app/shared/types/restaurant";
-import Button from "@/app/shared/ui/Button";
-import { useRouter } from "next/navigation";
+'use client';
+import { useToast } from '@/app/shared/hooks/useToast';
+import { Restaurant } from '@/app/shared/types/restaurant';
+import Button from '@/app/shared/ui/Button';
+import { useRouter } from 'next/navigation';
 
 export default function PopupActions({
   restaurant,
 }: {
   restaurant: Restaurant;
 }) {
+  const toast = useToast();
   const router = useRouter();
   const handleDetailPage = () => {
     // TODO: 여기서 디테일 페이지로 이동하는 로직을 작성
-    router.push(`/search/${restaurant.id}/detail`);
+    toast.success('상세보기 페이지로 이동합니다.');
+    router.push(`/restaurants/${restaurant.id}`);
   };
 
   const handleBaropotPage = () => {
-    // TODO: 여기서 바로팟 페이지로 이동하는 로직을 작성
-    router.push(`/restaurants/${restaurant.id}/baropot/create`);
+    // TODO: 07.23 현재 바로팟 상세페이지로 이동하기 전, /baropot으로 연결하여 전체리스트를 보여줄 수 있게 설정
+    toast.success('바로팟 페이지로 이동합니다.');
+    router.push('/baropot');
+    // router.push(`/restaurants/${restaurant.id}/baropot/create`); TODO: 기존경로
   };
 
   return (
     <div className="flex gap-2">
       <Button
         onClick={handleDetailPage}
-        className="flex-1 text-center bg-gray-100 text-gray-700 text-xs py-2 rounded-lg hover:bg-gray-200 transition"
+        className="flex-1 rounded-lg bg-gray-100 py-2 text-center text-xs text-gray-700 transition hover:bg-gray-200"
       >
         상세보기
       </Button>
       {restaurant.hasBaropot && (
         <Button
           onClick={handleBaropotPage}
-          className="flex-1 text-center bg-gradient-to-r from-orange-400 to-red-500 text-white text-xs py-2 rounded-lg hover:shadow-md transition"
+          className="flex-1 rounded-lg bg-gradient-to-r from-orange-400 to-red-500 py-2 text-center text-xs text-white transition hover:shadow-md"
         >
           바로팟 참여 🔥
         </Button>
