5d31d509388d8d34eb7e491000b9782ae317c9fc
이승준 <tmdwns092727@nate.com>
2025-07-23 14:57:33 +0900

    style: MapHeaderBar 디자인 수정
    - 검색 바 추가
    - 기존 헤더는 일단 삭제 조치

--- src/app/(afterLogin)/map/page.tsx
+++ src/app/(afterLogin)/map/page.tsx
@@ -1,12 +1,8 @@
-"use client";
-import KaKaoContainer from "@/app/features/map/components/KaKaoContainer";
-import { Header } from "@/app/shared/ui";
+import KaKaoContainer from '@/app/features/map/components/KaKaoContainer';
 
 export default function KakaoMapView() {
   return (
     <div className="min-h-screen bg-[#E6EEF5]">
-      {/* 헤더 */}
-      <Header title="지도" />
       <KaKaoContainer />
     </div>
   );
