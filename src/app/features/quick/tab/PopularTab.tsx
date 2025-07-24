d8cca0bd7ff02741326f5d24e949e4289b9916e5
이승준 <tmdwns092727@nate.com>
2025-07-24 22:22:18 +0900

    feat: Quick 탭 추가

---
+++ src/app/features/quick/tab/PopularTab.tsx
@@ -0,0 +1,13 @@
+import { useRestaurantList } from '@/app/shared/hooks/queries/useRestaurant';
+import React from 'react';
+
+export default function PopularTab() {
+  // TODO: 최근순
+  // TODO: 리뷰 점수가 많은 순
+  // const { data: restaurantList = [], isLoading } = useRestaurantList();
+  // const updatedRestaurant = restaurantList.map((item) => item.filter(
+  //   (item) => item.name !== restaurantList.name
+  // );)
+  // console.log(restaurantList);
+  return <div>PopularTab</div>;
+}
