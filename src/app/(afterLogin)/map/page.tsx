97ab35455fda7b9b9624327c02b769c6124d7016
이승준 <tmdwns092727@nate.com>
2025-07-29 14:50:40 +0900

    feat: 맵 페이지 SEO 추가

--- src/app/(afterLogin)/map/page.tsx
+++ src/app/(afterLogin)/map/page.tsx
@@ -1,4 +1,11 @@
 import KaKaoContainer from '@/app/features/map/components/KaKaoContainer';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '바로고 | 맛집이 고민이라면 지도에서 찾아보세요.',
+  description: '맛집을 검색하여 찾아보세요.',
+  keywords: '지도, 맛집 검색',
+};
 
 export default function KakaoMapView() {
   return (
