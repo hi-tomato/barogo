78fc8a4d54e42cdbf303b2c7a1a813d86de9f863
이승준 <tmdwns092727@nate.com>
2025-07-29 14:51:43 +0900

    feat: 검색 페이지 SEO 추가

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,4 +1,15 @@
-import { SearchPage } from "@/app/features/search";
+import { SearchPage } from '@/app/features/search';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '바로고 | 맛있는 맛집을 찾아보세요.',
+  description: '맛집들을 검색하여 찾아보세요.',
+  keywords: '맛집, 맛집 검색, 음식점',
+  openGraph: {
+    title: '바로고 검색 페이지',
+    description: '맛집들을 검색하여 찾아보세요.',
+  },
+};
 
 export default function page() {
   return <SearchPage />;
