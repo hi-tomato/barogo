e8ce5b1955bb952f11728698dfbceb4e1976f9da
이승준 <tmdwns092727@nate.com>
2025-07-29 14:48:39 +0900

    feat: 메인 페이지 SEO 추가
    - Server Component 수정, Client Component 추가

--- src/app/(afterLogin)/main/page.tsx
+++ src/app/(afterLogin)/main/page.tsx
@@ -1,5 +1,16 @@
 import MainHeader from '@/app/features/main/components/MainHeader';
 import MainContentsPage from '@/app/features/main/page/MainContentsPage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '바로고 | 나와 취향이 맞는분들과 맛집 투어를 함께 해요.',
+  description: '맛집 컨텐츠를 제공하는 바로고 메인 페이지 입니다.',
+  keywords: '맛집, 맛집 추천, 음식점, 바로고',
+  openGraph: {
+    title: '바로고 메인 페이지',
+    description: '맛집 컨텐츠를 제공하는 바로고 메인 페이지 입니다.',
+  },
+};
 
 export default function MainPages() {
   return (
