17c698c9e2f5fe00bf0f01de0f30d13556cec80f
이승준 <tmdwns092727@nate.com>
2025-08-01 11:28:22 +0900

    refactor: 회원가입 페이지 리팩토링
    - MetaData 추가 및 SSR 적용

---
+++ src/app/(beforeLogin)/register/types/index.ts
@@ -0,0 +1,39 @@
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+
+export interface RegisterFormData {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  terms: boolean;
+}
+
+interface ValidationRules {
+  name: {
+    required: string;
+    minLength: { value: number; message: string };
+  };
+  email: {
+    required: string;
+    pattern: { value: RegExp; message: string };
+  };
+  password: {
+    required: string;
+    minLength: { value: number; message: string };
+  };
+  confirmPassword: {
+    required: string;
+    validate: (value: string, password: string) => boolean | string;
+  };
+  terms: {
+    required: string;
+  };
+}
+
+export interface RegisterFormProps {
+  register: UseFormRegister<RegisterFormData>;
+  handleSubmit: () => void;
+  errors: FieldErrors<RegisterFormData>;
+  isSignUpPending: boolean;
+  validateRules: ValidationRules;
+}
