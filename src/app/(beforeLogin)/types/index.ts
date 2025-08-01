fdc497c229199b774f8bfb210daaf905e20d5c02
이승준 <tmdwns092727@nate.com>
2025-08-01 11:42:10 +0900

    refactor: Landing 페이지 리팩토링
    - MetaData 추가 및 SSR 적용

---
+++ src/app/(beforeLogin)/types/index.ts
@@ -0,0 +1,11 @@
+export interface Feature {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export interface FeatureCardProps {
+  icon: string;
+  title: string;
+  description: string;
+}
